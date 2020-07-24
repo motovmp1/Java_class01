cb8b3e6a790091b0f7f41c361704af45fd83f685
motovmp1 <vinicius.mpinho@gmail.com>
2020-07-24 10:17:39 +0800

    Running test 1

---
+++ src/chapter2/GrossPayCalculator.java
@@ -0,0 +1,30 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+
+    public static void main(String[] args)
+    {
+        //1. get the number of hours
+        System.out.println("Enter the value of hour the employee worked");
+        Scanner scanner = new Scanner(System.in);
+        int hours = scanner.nextInt();
+
+
+        //2. get the hourly pay rate
+        System.out.println("Enter the employee pay rate");
+        double pay = scanner.nextDouble();
+        scanner.close();
+
+        //3. Multiply hours and pay rate
+        double grossPay = (hours * pay);
+
+
+        // 4. Display the result
+        System.out.println("This is a the final salary: " + grossPay);
+
+
+    }
+
+}
